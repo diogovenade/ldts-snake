2130749970a464c7eccfee57cd96c760194fb447
Diogo Venade <dmsvenade@gmail.com>
2023-12-21 19:48:17 +0000

    Added GameOverViewerTest

---
+++ src/test/java/viewer/menu/GameOverViewerTest.java
@@ -0,0 +1,27 @@
+package viewer.menu;
+
+import l03gr05.gui.LanternaGUI;
+import l03gr05.model.menu.GameOver;
+import l03gr05.viewer.menu.GameOverViewer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class GameOverViewerTest {
+    private GameOverViewer gameOverViewer;
+    private LanternaGUI gui;
+
+    @BeforeEach
+    public void setUp() {
+        gameOverViewer = new GameOverViewer(new GameOver(23));
+        gui = mock(LanternaGUI.class);
+    }
+
+    @Test
+    public void testDrawElements() {
+        gameOverViewer.drawElements(gui);
+        verify(gui, times(4)).drawText(any(), any(), any());
+    }
+}
